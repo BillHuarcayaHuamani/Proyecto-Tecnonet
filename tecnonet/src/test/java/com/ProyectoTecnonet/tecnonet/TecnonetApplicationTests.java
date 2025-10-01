e54d31aea320cfa0f1aa33e30f3793503a0e0819
BillHuarcayaHuamani <billonario321@gmail.com>
2025-09-30 19:07:06 -0500

    Actualización módulos

---
+++ tecnonet/src/test/java/com/ProyectoTecnonet/tecnonet/TecnonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoTecnonet.tecnonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
